4379a0f0e2107c7947f247d90582a23e4ff839d7
Gustavo780904 <gustavodutrasantos@gmail.com>
2021-10-19 15:42:34 -0300

    calculadora funcionando

--- SD/src/projeto6/Calculator.java
+++ SD/src/projeto6/Calculator.java
@@ -1,5 +1,8 @@
 package projeto6;
 
+import static javax.swing.GroupLayout.DEFAULT_SIZE;
+import static javax.swing.GroupLayout.PREFERRED_SIZE;
+
 import java.net.MalformedURLException;
 import java.rmi.Naming;
 import java.rmi.NotBoundException;
@@ -9,8 +12,6 @@ import java.util.logging.Logger;
 
 import javax.swing.GroupLayout;
 import javax.swing.GroupLayout.Alignment;
-import static javax.swing.GroupLayout.DEFAULT_SIZE;
-import static javax.swing.GroupLayout.PREFERRED_SIZE;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -78,73 +79,60 @@ public class Calculator extends JFrame {
 
 		// botão 1
 		buttonOne = new JButton("1");
-		count += "1";
-		visor.setText(count);
+		buttonOne.addActionListener(e -> jButtonOne(null));
+			
 
 		// botão 2
 		buttonTwo = new JButton("2");
-		count += "2";
-		visor.setText(count);
+		buttonTwo.addActionListener(e -> jButtonTwo(null));
 
 		// botão 3
 		buttonThree = new JButton("3");
-		count += "3";
-		visor.setText(count);
+		buttonThree.addActionListener(e -> jButtonThree(null));
 
 		// botão 4
 		buttonFour = new JButton("4");
-		count += "4";
-		visor.setText(count);
+		buttonFour.addActionListener(e -> jButtonFour(null));
 
 		// botão 5
 		buttonFive = new JButton("5");
-		count += "5";
-		visor.setText(count);
+		buttonFive.addActionListener(e -> jButtonFive(null));
 
 		// botão 6
 		buttonSix = new JButton("6");
-		count += "6";
-		visor.setText(count);
+		buttonSix.addActionListener(e -> jButtonSix(null));
 
 		// botão 7
 		buttonSeven = new JButton("7");
-		count += "7";
-		visor.setText(count);
+		buttonSeven.addActionListener(e -> jButtonSeven(null));
 
 		// botão 8
 		buttonEight = new JButton("8");
-		count += "8";
-		visor.setText(count);
+		buttonEight.addActionListener(e -> jButtonEight(null));
 
 		// botão 9
 		buttonNine = new JButton("9");
-		count += "9";
-		visor.setText(count);
+		buttonNine.addActionListener(e -> jButtonNine(null));
 
 		// botão 0
 		buttonZero = new JButton("0");
-		count += "0";
-		visor.setText(count);
+		buttonZero.addActionListener(e -> jButtonZero(null));
 
 		// botão "soma"
-		sum = new JButton();
-		count += "+";
-		visor.setText(count);
+		sum = new JButton("+");
+		sum.addActionListener(e -> jSum(null));
 
 		// botão "subtração"
 		subtraction = new JButton("-");
-		count += "-";
-		visor.setText(count);
+		subtraction.addActionListener(e -> jSubtraction(null));
 
 		// botão "multiplicação"
 		multiplication = new JButton("x");
-		count += "x";
-		visor.setText(count);
+		multiplication.addActionListener(e -> jMultiplication(null));
 
 		// botão "divisão"
 		division = new JButton("/");
-		count += "/";
-		visor.setText(count);
+		division.addActionListener(e -> jDivision(null));
 
 		// botão "igual"
 		equal = new JButton("=");
@@ -152,8 +140,7 @@ public class Calculator extends JFrame {
 
 		// botão "limpar"
 		clean = new JButton("Limpar");
-		count = "";
-		visor.setText(count);
+		clean.addActionListener(e -> jClean(null));
 
 		// layout dos componentes
 		var layout = new GroupLayout(getContentPane());
@@ -189,33 +176,101 @@ public class Calculator extends JFrame {
 		layout.setVerticalGroup(layout.createSequentialGroup()
 						.addComponent(visor, PREFERRED_SIZE, PREFERRED_SIZE, PREFERRED_SIZE)
 						.addGroup(layout.createParallelGroup(Alignment.CENTER)
-						.addComponent(buttonOne, PREFERRED_SIZE, DEFAULT_SIZE, PREFERRED_SIZE)
+								.addComponent(buttonOne, PREFERRED_SIZE, DEFAULT_SIZE, PREFERRED_SIZE)
+								.addComponent(buttonTwo, PREFERRED_SIZE, DEFAULT_SIZE, PREFERRED_SIZE)
+								.addComponent(buttonThree, PREFERRED_SIZE, DEFAULT_SIZE, PREFERRED_SIZE)
+								.addComponent(sum, PREFERRED_SIZE, DEFAULT_SIZE, PREFERRED_SIZE)
+				)
+				.addGroup(layout.createParallelGroup(Alignment.CENTER)
 						.addComponent(buttonFour, PREFERRED_SIZE, DEFAULT_SIZE, PREFERRED_SIZE)
-						.addComponent(buttonSeven, PREFERRED_SIZE, DEFAULT_SIZE, PREFERRED_SIZE)
-				)
-				.addGroup(layout.createParallelGroup(Alignment.CENTER)
-						.addComponent(buttonTwo, PREFERRED_SIZE, DEFAULT_SIZE, PREFERRED_SIZE)
 						.addComponent(buttonFive, PREFERRED_SIZE, DEFAULT_SIZE, PREFERRED_SIZE)
-						.addComponent(buttonEight, PREFERRED_SIZE, DEFAULT_SIZE, PREFERRED_SIZE)
-						.addComponent(buttonZero, PREFERRED_SIZE, DEFAULT_SIZE, PREFERRED_SIZE)
-						.addComponent(clean, PREFERRED_SIZE, DEFAULT_SIZE, PREFERRED_SIZE)
+						.addComponent(buttonSix, PREFERRED_SIZE, DEFAULT_SIZE, PREFERRED_SIZE)
+						.addComponent(subtraction, PREFERRED_SIZE, DEFAULT_SIZE, PREFERRED_SIZE)
 				)
 				.addGroup(layout.createParallelGroup(Alignment.CENTER)
 						.addComponent(buttonSeven, PREFERRED_SIZE, DEFAULT_SIZE, PREFERRED_SIZE)
-						.addComponent(buttonSix, PREFERRED_SIZE, DEFAULT_SIZE, PREFERRED_SIZE)
+						.addComponent(buttonEight, PREFERRED_SIZE, DEFAULT_SIZE, PREFERRED_SIZE)
 						.addComponent(buttonNine, PREFERRED_SIZE, DEFAULT_SIZE, PREFERRED_SIZE)
+						.addComponent(multiplication, PREFERRED_SIZE, DEFAULT_SIZE, PREFERRED_SIZE)
 				)
 				.addGroup(layout.createParallelGroup(Alignment.CENTER)
-						.addComponent(sum, PREFERRED_SIZE, DEFAULT_SIZE, PREFERRED_SIZE)
-						.addComponent(subtraction, PREFERRED_SIZE, DEFAULT_SIZE, PREFERRED_SIZE)
-						.addComponent(multiplication, PREFERRED_SIZE, DEFAULT_SIZE, PREFERRED_SIZE)
+						.addComponent(buttonZero, PREFERRED_SIZE, DEFAULT_SIZE, PREFERRED_SIZE)
 						.addComponent(division, PREFERRED_SIZE, DEFAULT_SIZE, PREFERRED_SIZE)
-						.addComponent(equal, PREFERRED_SIZE, DEFAULT_SIZE, PREFERRED_SIZE)
 				)
+				.addGroup(layout.createParallelGroup(Alignment.CENTER)
+						.addComponent(clean, PREFERRED_SIZE, DEFAULT_SIZE, PREFERRED_SIZE)
+						.addComponent(equal, PREFERRED_SIZE, DEFAULT_SIZE, PREFERRED_SIZE)
+						)
 						
 			);
 	}
 
+	private void jButtonOne (java.awt.ActiveEvent evt) {
+		count += "1";
+		visor.setText(count);
+	}
+	private void jButtonTwo (java.awt.ActiveEvent evt) {
+		count += "2";
+		visor.setText(count);
+	}
+	private void jButtonThree (java.awt.ActiveEvent evt) {
+		count += "3";
+		visor.setText(count);
+	}
+	private void jButtonFour (java.awt.ActiveEvent evt) {
+		count += "4";
+		visor.setText(count);
+	}
+	private void jButtonFive (java.awt.ActiveEvent evt) {
+		count += "5";
+		visor.setText(count);
+	}
+	private void jButtonSix (java.awt.ActiveEvent evt) {
+		count += "6";
+		visor.setText(count);
+	}
+	private void jButtonSeven (java.awt.ActiveEvent evt) {
+		count += "7";
+		visor.setText(count);
+	}
+	private void jButtonEight (java.awt.ActiveEvent evt) {
+		count += "8";
+		visor.setText(count);
+	}
+	private void jButtonNine (java.awt.ActiveEvent evt) {
+		count += "9";
+		visor.setText(count);
+	}
+	private void jButtonZero (java.awt.ActiveEvent evt) {
+		count += "0";
+		visor.setText(count);
+	}
+	private void jSum (java.awt.ActiveEvent evt) {
+		count += "+";
+		visor.setText(count);
+	}
+	private void jSubtraction (java.awt.ActiveEvent evt) {
+		count += "-";
+		visor.setText(count);
+	}
+	private void jMultiplication (java.awt.ActiveEvent evt) {
+		count += "x";
+		visor.setText(count);
+	}
+	private void jDivision (java.awt.ActiveEvent evt) {
+		count += "/";
+		visor.setText(count);
+	}
+	private void jClean (java.awt.ActiveEvent evt) {
+		count = "";
+		visor.setText(count);
+	}
+	private void jEqual (java.awt.ActiveEvent evt) {
+		count += "x";
+		visor.setText(count);
+	}
+	
+	
 	private void result() {
 
 		if (count.contains("+")) {
